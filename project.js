e3ce7b8e7179d7fede4fadaa8a7b1225483eb097
Wilawan Pond <79263616+wilawanpond@users.noreply.github.com>
2023-11-03 19:38:39 +0700

    Wrote things to do

---
+++ project.js
@@ -0,0 +1,10 @@
+/*
+Things to do!
+    1. Player deposits some money
+    2. Determine number of lines to bet on
+    3. Collect a bet amount
+    4. Spin the slot machine
+    5. Checking if the player won
+    6. Give the user their winnings
+    7. Play again!
+*/
