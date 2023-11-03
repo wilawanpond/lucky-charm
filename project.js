78d0e58b88cfbfe790222f1e4c1ed79bfcc874c7
Wilawan Pond <79263616+wilawanpond@users.noreply.github.com>
2023-11-04 02:26:30 +0700

    Created a function for printing slot machine

--- project.js
+++ project.js
@@ -135,13 +135,32 @@ const transpose = (reels) => {
     for (let i = 0; i < ROWS; i++) {
         rows.push([]);
         for (let j = 0; j < COLS; j++) {
-            rows[i].push([j][i]);
+            rows[i].push(reels[j][i]);
         }
     }
-}
+    return rows;
+};
+
+// Create a function for printing a slot machine
+const printSlotMachine = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol; // add symbol into rowString
+            // Adding the pipe operator if it is not the last symbol
+            if (i != row.length - 1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+};
+
 
 
 let balance = deposit(); // Change the variable from const to let, so we can change the value
 const numberOfLines = getNumberOfLines(); // Call the getNumberOfLines function to be stored in the constant
 const bet = getBet(balance, numberOfLines); // Call the getBet function to be stored in the constant
 const reels = spin();
+const rows = transpose(reels);
+printSlotMachine(rows);
